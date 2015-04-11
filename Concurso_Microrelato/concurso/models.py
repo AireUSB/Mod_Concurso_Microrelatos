6afa787b971bc7cd1f6fdde5a85979df78993dd8
Bernardo <bernardojm91@github.com>
2015-04-11 12:52:29 -0430

    implementados metodos para: la captacion de tweets por streaming, creacion y activacion de hilo demonio responsable de persistencia en la verificacion de tweets y su respectivo desactivador

--- Concurso_Microrelato/concurso/models.py
+++ Concurso_Microrelato/concurso/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 import twitter
+import time
+import threading
+from twitter import Status
 # Create your models here.
 #clase q representa un tweet para Django y la DB
 class tweetCargado(models.Model):
@@ -18,7 +21,7 @@ class tweetCargado(models.Model):
     def aprobarTweet(self, api):
     	self.estado='A'
     	self.save()
-    	#api.PostRetweet(self.idRef) #se retwitea si se aprueba
+    	api.PostRetweet(self.idRef) #se retwitea si se aprueba
 
 	def rechazarTweet(self):
 		self.estado='R'
@@ -65,6 +68,7 @@ def updateRtCount():
 	api = loginConcurso()
 	for rtTweet in tweetsAprobados:
 		rtTweet.rtCount=api.GetStatus(id=rtTweet.idRef).retweet_count
+		rtTweet.save()
 		return(tweetsAprobados)
 
 #devuelve lista con tweets aprobados ordenados por su rtCount y actualizado
@@ -73,19 +77,50 @@ def getTopRt():
 	aprobados.order_by('rtCount')
 	return (aprobados)
 
+#imprime lista de twitter.status
 def printStatusList(statusList):
 	for stuff in statusList:
 		print(stuff.id)
 
-"""while (True):
-    stream = apiTest.GetStreamFilter(None, ['someWord'])
-    try:
-        print(stream.next())
-    except:
-        print ("No posts")
+#borra todos los tweets en la DB
+def deleteAllTweets():
+	toDelete=tweetCargado.objects.all()
+	for deleted in toDelete:
+		deleted.delete()
 
-    time.sleep(3)
-    """
+#variables de control sobre hilo demonio
+daemonThread = None
+daemonStatus = 0
+
+#metodo del hilo demonio para tweet streaming
+def startTweetStream(palabra,api):
+	print "Captando tweets"
+	for tweet in api.GetStreamFilter(track=[palabra]):
+		lastTweet = Status.NewFromJsonDict(tweet)
+		statusToTweet(lastTweet.id,lastTweet.user.screen_name,lastTweet.text)
+		print lastTweet.text
+		if (daemonStatus == 0):
+			print "Captacion de Tweets detenida"
+			return
+
+#activa hilo demonio a captar tweets con la palabra 'busqueda'
+def startDaemonThread(busqueda):
+	api=loginConcurso()
+	try:
+		global daemonStatus
+		daemonStatus = 1
+		global daemonThread
+		daemonThread = threading.Thread(target=startTweetStream, args=[busqueda,api])
+		daemonThread.start()
+	except:
+		print "Error: unable to start thread"
+
+#desactiva hilo demonio
+def stopDaemonThread():
+	global daemonStatus
+	daemonStatus = 0
+	return
+
 
 
 
