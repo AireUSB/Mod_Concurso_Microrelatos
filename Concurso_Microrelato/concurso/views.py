78b5431a0c987e207230301a6f298828e7b360be
Bernardo <bernardojm91@github.com>
2015-04-13 22:12:46 -0430

    solucionado problema de dependencias entre ficheros, Hello World html

--- Concurso_Microrelato/concurso/views.py
+++ Concurso_Microrelato/concurso/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'concurso/static/index.html')
+    return render(request, 'concurso/index.html')
